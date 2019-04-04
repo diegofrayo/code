a2807a2cc6490af02307109ac412788009d4a5e3
Diego Rayo <diegofrayo@gmail.com>
2019-04-04 15:21:36 -0500

    :rocket: [fix] redirects config

--- build.js
+++ build.js
@@ -6,3 +6,4 @@ shell.exec('yarn install');
 shell.exec('yarn build');
 
 fs.copyFileSync('./static/sign-in.html', './static/public/sign-in.html');
+fs.copyFileSync('./static/netlify.toml', './static/public/netlify.toml');
