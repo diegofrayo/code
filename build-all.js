ee11850a06d272ff687e12da97b23091f746d81b
Diego Rayo <diegofrayo@gmail.com>
2019-04-07 18:51:30 -0500

    :rocket: [improve] build docs script

--- build-all.js
+++
@@ -1,8 +0,0 @@
-const shell = require('shelljs');
-
-shell.cd('packages/docs');
-
-['components', 'form', 'styles', 'validator'].forEach(project => {
-  shell.cd(`../${project}`);
-  shell.exec('yarn build');
-});
--- build.js
+++ build.js
@@ -1,9 +1,50 @@
-const shell = require('shelljs');
 const fs = require('fs');
+const shell = require('shelljs');
+const { argv } = require('yargs');
 
-shell.cd('packages/docs');
-shell.exec('yarn install');
-shell.exec('yarn build');
+const PROJECTS = ['components', 'form', 'styles', 'validator', 'docs'];
 
-fs.copyFileSync('./static/sign-in.html', './static/public/sign-in.html');
-fs.copyFileSync('./static/netlify.toml', './static/public/netlify.toml');
+const buildDocs = () => {
+  shell.cd('packages/docs');
+
+  PROJECTS.forEach(project => {
+    shell.cd(`../${project}`);
+    shell.exec('yarn install');
+  });
+
+  PROJECTS.forEach(project => {
+    shell.cd(`../${project}`);
+    shell.exec('yarn build');
+  });
+
+  fs.copyFileSync('./static/sign-in.html', './static/public/sign-in.html');
+  fs.copyFileSync('./static/netlify.toml', './static/public/netlify.toml');
+};
+
+const buildAll = () => {
+  shell.cd('packages/docs');
+
+  PROJECTS.filter(p => p !== 'docs').forEach(project => {
+    shell.cd(`../${project}`);
+    shell.exec('yarn build');
+  });
+};
+
+const lintAll = () => {
+  shell.cd('packages/docs');
+
+  PROJECTS.forEach(project => {
+    shell.cd(`../${project}`);
+    shell.exec('yarn lint');
+  });
+};
+
+if (argv['build-docs']) {
+  buildDocs();
+} else if (argv['build-all']) {
+  buildAll();
+} else if (argv['lint-all']) {
+  lintAll();
+} else {
+  console.log('Please type an available option');
+}
